d708f8966e02a1549445b024545cffa20c21fc5a
Matheus Lunardelli <matheusoliveiralunardelli@gmail.com>
2025-08-11 15:57:58 -0300

    alterações

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,8 +1,13 @@
 /// <reference types="cypress" />
 
+
 context('funcionalidade login', () =>{
-    it('deve fazer login com sucesso', ()=>{
+
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    it('deve fazer login com sucesso', ()=>{
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -11,14 +16,12 @@ context('funcionalidade login', () =>{
     })
 
     it('deve exibir mensagem de erro ao inserir usuario inválido', ()=>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@')
         cy.get('#password').type('teste@teste.com')
-        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-form > .button').click() 
     })
 
     it('deve exibir mensagem de erro ao inserir senha inválida', ()=>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@')
         cy.get('.woocommerce-form > .button').click()
---
+++ cypress/e2e/produtos.spec.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade da página de produtos', () => {
+
+    before(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]').first().click()
+        //cy.get('[class="product-block grid"]').last().click() - pega o ultimo da lista
+        //cy.get('[class="product-block grid"]').eq(4).click() - pega o quarto item da lista
+        //cy.get('[class="product-block grid"]').contains('Atlas Fitness Tank').click() - pega o item pelo nome
+    });
+
+    //Somente este cenario será executado .only()
+    it.only('Deve adicionar um produto ao carrinho', () => {
+
+        var quantidade = 3
+
+        cy.get('[class="product-block grid"]').eq(7).click()
+        cy.get('.button-variable-item-L').click() //Seleciona o tamanho L
+        cy.get('.button-variable-item-Purple').click() //Seleciona a cor roxa
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade) // Verifica se a quantidade foi adicionada
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.') //concatena a variavel com a mensagem
+
+
+        
+    });
+});
